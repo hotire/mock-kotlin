b66f940aee8070cbf9fe331ee771e7d2e9c435d3
hotire <gngh0101@gmail.com>
2021-10-12 00:29:55 +0900

    Rename

--- src/main/kotlin/com/github/hotire/mock/kotlin/Application.kt
+++ src/main/kotlin/com/github/hotire/mock/kotlin/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MockKotlinApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<MockKotlinApplication>(*args)
+    runApplication<Application>(*args)
 }
